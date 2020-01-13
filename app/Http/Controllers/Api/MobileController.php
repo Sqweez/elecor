e69f342f70844aa47bdb7146a26c9b9e977c342b
Sqweez <sasha.katerinin@gmail.com>
2020-01-13 17:55:27 +0600

    13/01/2020 SMS Service

--- app/Http/Controllers/Api/MobileController.php
+++ app/Http/Controllers/Api/MobileController.php
@@ -12,76 +12,9 @@ use App\Message;
 use App\MobileService;
 use App\Phone;
 use Illuminate\Http\Request;
+use Mobizon\MobizonApi;
 
 class MobileController extends Controller {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index() {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create() {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request) {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id) {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id) {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id) {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id) {
-        //
-    }
 
     public function sms(Request $request) {
         $phone = $request->get('phone');
@@ -94,7 +27,7 @@ class MobileController extends Controller {
 
         $this->sendMessage($phone, $code);
         $client = Client::find($clientPhone['client_id'])->only(['id']);
-        return ['code' => 1234, 'client_id' => $client['id']];
+        return ['code' => $code, 'client_id' => $client['id']];
     }
 
     public function getClientData(Client $client) {
@@ -105,20 +38,10 @@ class MobileController extends Controller {
 
     private function sendMessage($phone, $code) {
 
+        $api = new MobizonApi('kze2629bf716d2bff8b28880881e32bdc4e89b33526f76cad0aad9af7c45d964a73444', 'api.mobizon.kz');
         $_phone = '7' . substr($phone, 1);
-
-        $url = "https://api.mobizon.kz/service/message/sendSmsMessage?output=json&api=v1&apiKey=kzcfc1bce637a2d0c9191444d06da1c39394e999c69e806544b254af3afb5f8288d11c&recipient=77059864410&text=Пароль для мобильного приложения";
-
-        $params = "&recipient=77059864410&text=Пароль для мобильного приложения";
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//для возврата результата в виде строки, вместо прямого вывода в браузер
-        $returned = curl_exec($ch);
-        curl_close($ch);
-
-        return $returned;
-
+        $api->call('message', 'sendSMSMessage', array('recipient' => $_phone, 'text' => "Код подтверждения ELECOR: " . $code));
+        return true;
     }
 
     public function messages(Client $client) {
@@ -164,10 +87,7 @@ class MobileController extends Controller {
     }
 
     public function welcome(Client $client) {
-        $message = [
-            'title' => 'Добро пожаловать!',
-            'body' => 'Добро пожаловать в приложение Клиент ОА «ELECOR»!'
-        ];
+        $message = ['title' => 'Добро пожаловать!', 'body' => 'Добро пожаловать в приложение Клиент ОА «ELECOR»!'];
         $pushToken = $client['push_token'];
         $this->sendPush($message, $pushToken);
         $this->storeMessage(['client_id' => $client['id'], 'title' => $message['title'], 'body' => $message['body']]);
@@ -192,39 +112,21 @@ class MobileController extends Controller {
     }
 
     private function sendPush($message, $pushToken = null) {
-        $content = array(
-            "en" => $message['body']
-        );
+        $content = array("en" => $message['body']);
 
-        $heading = [
-            "en" => $message['title']
-        ];
+        $heading = ["en" => $message['title']];
 
-        $segment = !$pushToken
-            ? ['included_segments' => array('All')]
-            : gettype($pushToken) === 'array'
-                ? ['include_player_ids' => $pushToken]
-                : ['include_player_ids' => [$pushToken]];
+        $segment = !$pushToken ? ['included_segments' => array('All')] : gettype($pushToken) === 'array' ? ['include_player_ids' => $pushToken] : ['include_player_ids' => [$pushToken]];
 
         $key = array_keys($segment)[0];
 
-        $fields = array(
-            'app_id' => env('ONE_SIGNAL_APP_ID'),
-            'data' => array("foo" => "bar"),
-            $key => $segment[$key],
-            'small_icon' => "ic_stat_onesignal_default.png",
-            'large_icon' =>"ic_stat_onesignal_default.png",
-            'contents' => $content,
-            'headings' => $heading,
-            'android_group' => 'group'
-        );
+        $fields = array('app_id' => env('ONE_SIGNAL_APP_ID'), 'data' => array("foo" => "bar"), $key => $segment[$key], 'small_icon' => "ic_stat_onesignal_default.png", 'large_icon' => "ic_stat_onesignal_default.png", 'contents' => $content, 'headings' => $heading, 'android_group' => 'group');
 
 
         $fields = json_encode($fields);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
-            'Authorization: Basic ' . env("ONE_SIGNAL_REST_API_KEY")));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8', 'Authorization: Basic ' . env("ONE_SIGNAL_REST_API_KEY")));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
         curl_setopt($ch, CURLOPT_POST, TRUE);
